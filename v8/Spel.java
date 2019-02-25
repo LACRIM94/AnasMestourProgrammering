08fb831ccf896bf6337d987553a253ea73009b2d
Anas Mestour <anasmestour@gmail.com>
2019-02-25 10:57:49 +0100

    Update Spel.java

--- v8/Spel.java
+++ v8/Spel.java
@@ -9,13 +9,13 @@ public class Spel {
 	
 		Random rand = new Random(); 
 
-		int numretsomgissas = rand.nextInt(100);
+		int numretsomgissas = rand.nextInt(101);
 		int försök = 0;
 		Scanner input = new Scanner (System.in);
 		int gissa;
 		boolean vinst = false;
 		
-		while (vinst == false) {
+		while (vinst == false || försök == 5) {
 		
 			System.out.println("Gissa ett nummer mellan 1 och 100: ");
 		    gissa = input.nextInt();
@@ -23,13 +23,26 @@ public class Spel {
 		    
 		    if (gissa == numretsomgissas) {
 		        vinst = true;
+		        System.out.println("Du vann");
+				System.out.println("Numret var " + numretsomgissas);
+				System.out.println("Det tog dig " + försök + " försök");
+				    
 		    }   
 		    else if (gissa < numretsomgissas) {
-		    System.out.println("Din gissning är för lågt");
+		    System.out.println("Din gissning är för låg");
 		 
 		    }
+		    else if (gissa > numretsomgissas) {
+			    System.out.println("Din gissning är för hög");
+			 
+			    }
+		    if (försök == 5) {
+		    System.out.println("Du förlorade, max antalet försök är 5");
+		    System.out.println("Numret var " + numretsomgissas);
+		    break;
+	
 		    
 		}
-	}
-
-}
+		
+		
+} } }
